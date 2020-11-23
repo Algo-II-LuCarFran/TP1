07b19674b9a5871aaa729daa76802affa1447787
Lucas Burna <burna680@users.noreply.github.com>
2020-11-23 15:36:45 -0300

    Clase Lista
    Lista para prueba de fuga de memoria.
    Faltan implementar los operadores básicos:
    >>
    <<
    +

---
+++ Codigos/Ejercicio_1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "Lista.h"
+using namespace std;
+
+int main()
+{
+    cout<<"Comienzo de la prueba"<<endl;
+    list <int> L;
+    cout<<"Se pudo crear una lista"<<endl;
+    L.insert(1);
+    cout<<"Se pudo insertar al final?"<<endl;
+
+    return 0;
+}
