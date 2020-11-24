899d8c58675113224e64a53a700f1234658bf818
Lucas Burna <burna680@users.noreply.github.com>
2020-11-23 22:24:40 -0300

    Actualizacion de la branch
    Codigo actualizado 23/11/20.
    -Agregado de constructor de txn en base a un array de strings (incompleto)
    -cmd Transfer incompleto

---
+++ Codigos/Lista.h
@@ -0,0 +1,341 @@
+// Implementacion de list doblemente enlazada no circular utilizando
+// templates e iteradores.
+
+//Se aprovecha el hecho de que la lista tiene una referencia del principio y el final para reducir a la mitad
+//el procesamiento de las altas,bajas y borrado de las listas.
+
+//Los nodos son creados con memoria dinamica.
+
+#ifndef _LIST_H_
+#define _LIST_H_
+#include <iostream>
+#include <math.h> //Necesaria para el uso de floor().
+using namespace std;
+
+template<typename T>
+class list
+{
+    class node
+    {
+        // Debido al fuerte acople entre iteradores y la estructura a
+		// la cual iteran (es decir, el iterador necesita conocer los
+		// detalles de implementación del TDA para poder abstraer el
+		// recorrido), permitimos que la clase iterador tenga acceso
+		// directo a los detalles internos del TDA.
+
+        // La list puede tener  acceso a los atributos privados del nodo para poder 
+        //modificarlos, dado que la clase nodo es inherente a la clase list.
+
+        T data;
+        node* next;
+        node* prev;
+
+        friend class list;       
+        //friend class iterador;
+
+        public:
+        node(T const &data_){data=data_ ; next= NULL; prev=NULL;}; //Constructor a partir de dato. 
+        //Se asume que siempre que se quiera crear un nodo se tendrá la informacion que va a contener.
+        ~node(){}; //Destructor.
+    };
+
+    node* first;
+    node* last;
+    // node* iterator;
+    size_t max_size; //Tamaño de la lista.Si la lista no esta vacia max_size>=1
+                    // Si la lista esta vacia max_size=0
+
+    public:
+    list(); //Constructor basico
+    list(const list& L); //Constructor en base a otra list.
+    ~list(); //Destructor
+    void append(const T& t); //Agregar nodo al final de la list.
+    void insert(const T& t); //Agregar nodo al principio de la list.
+    bool placeElement(const T& t, size_t n=1);//Agregar nodo en la posicion n de la list. Si no lo pudo agregar
+                                             //devuelve false. Por defecto lo agrega al principio.
+    bool empty(); //Verifica si la list está vacia.
+    T* find(const T& t); //Encuentra el nodo que contiene el dato T. Si  no lo encuentra, devuelve NULL.
+    bool removeElement(const T& t); //Elimina el primer nodo que contiene al dato t. Devuelve false si no pudo eliminarlo.
+    size_t size(); //Obtiene el tamaño de la list
+
+    template<typename TT>
+    friend std::ostream &operator<<(std::ostream&, list<TT> &); //Operador de impresion de salida
+    // list const &operator=(const list& other_list);
+};
+
+template<typename T>
+list<T>::list(){first=NULL;last=NULL;max_size=0;}
+
+template<typename T>
+list<T>::list(const list& L)
+{
+    node* prev_;
+    node* next_;
+    size_t i;
+
+    if(L.empty())
+    {
+        this->max_size=0;
+        this->first=NULL;
+        this->last=NULL;
+    }
+    else
+    {
+        this->first=L->first;
+        this->last=L->last;
+
+        next_=L->first;
+        prev_=L->last;
+        for(size_t i=1; i<=floor(L->max_size)/2;i++)
+        {    
+            node* iter_1=new node(next_->data);
+            iter_1=next_;
+            next_=iter_1->next;
+            next_->prev=iter_1;
+
+            node* iter_2=new node(prev_->data);
+            iter_2=prev_;
+            prev_=iter_2->prev;
+            prev_->next=iter_2;
+        }
+        if((L->max_size)%2) //En este punto next->next=prev_->prev
+        {
+            node* iter_1=new node(next_->next->data);
+            iter_1=next_;
+            next_=iter_1->next;
+            next_->prev=iter_1;
+        } 
+        this->max_size=L->max_size;
+    }
+}
+
+template<typename T>
+bool list<T>::empty()
+{
+    //Forma complicada de preguntar si la lista esta vacia
+    // if((this->first==this->last)&&(this->first==NULL)) 
+    if(!max_size)
+        return true;
+    return false;
+
+}
+
+
+template<typename T>
+list<T>::~list() //Verficar que elimine TODOS los nodos de la lista.
+{
+    size_t i,j;
+    node* aux_forwards;
+    node* aux_backwards;
+    //Para borrar la lista doblemente enlazada se procede a eliminar
+    //los nodos desde el principio hasta la mitad y desde el final hacia 
+    //la mitad:
+    // - se inicializa un puntero auxiliar al final de la lista y otro al principio;
+    // - se hace que los punteros auxiliares apunten a first y last de la lista original;
+    // - se borran first y last de la lista original;
+    // - se designan los punteros auxiliares como first y last de la "nueva lista" con menos nodos;
+    // - si el numero de elementos en la lista era impar, se elimina el sobrante.
+     
+    for(i=1,j=max_size; i<=floor(max_size)/2 ; i++,j--) //La presencia de i y j son meramente representativas, para dar una analogia con arreglos.
+    {
+        aux_forwards=this->first->next;
+        aux_backwards=this->last->prev;
+
+        delete (this->first);
+        delete (this->last);
+
+        this->first=aux_forwards;
+        this->last=aux_backwards;
+    }
+
+    if((this->max_size)%2)
+    {
+        delete this->first;
+    }
+}
+
+template<typename T>
+size_t list<T>::size(){return this->max_size;};
+
+template<typename T>
+void list<T>::append(const T& t)
+{
+    node *aux= new node(t);
+
+    if( this->empty() )
+    {
+        aux->next=NULL;
+        aux->prev=NULL;
+        this->last=aux;
+        this->first=aux;
+    }
+    else
+    {
+        aux->next=NULL;
+        aux->prev=this->last;
+        this->last=aux;
+    }
+    this->max_size=this->max_size+1;
+
+}
+
+template<typename T>
+void list<T>::insert(const T& t)
+{
+    node *aux= new node(t);
+
+    if( this->empty() )
+    {
+        aux->next=NULL;
+        aux->prev=NULL;
+        this->last=aux;
+        this->first=aux;
+    }
+    else
+    {
+        aux->prev=NULL;
+        aux->next=this->first;
+        this->first=aux;
+    }
+    this->max_size=this->max_size+1;
+
+}
+
+template <typename T>
+bool list<T>::placeElement(const T& t, size_t n)
+{
+    if(n>max_size)
+        return false;
+
+    node*aux=new node(t);
+    node* p_aux;
+
+    if(n==1)
+        this->insert(t);
+    else if(n==this->max_size)
+        this->append(t);
+    else if(n<=floor(this->max_size)/2)
+    {    
+        aux=this->first;
+        aux->prev=NULL;
+        for(size_t i=1; i<n ;i++)
+        {    
+            p_aux=aux;
+            aux=aux->next;
+            aux->prev=p_aux;
+        }
+    }
+    else if(n>floor(this->max_size)/2)
+    {    
+        aux=this->last;
+        aux->next=NULL;
+        for(size_t i=1; i<n ;i++)
+        {    
+            p_aux=aux;
+            aux=aux->prev;
+            aux->next=p_aux;
+        }
+    }
+    else 
+        return false; //Si por algún motivo no pudo realizarse ninguna de las acciones previas, devuelve error.
+    return true;
+}
+
+
+template<typename T>
+T* list<T>::find(const T& t)
+{
+    node* prev_;
+    node* next_;
+    node* iter;
+
+    size_t i;
+
+    if(this->empty())
+        return NULL;
+    else
+    {
+        next_=this->first;
+        prev_=this->last;
+        for(size_t i=1; i<=floor(this->max_size)/2;i++)
+        {    
+            if(next_->data==t)
+                return next_;
+            if(prev_->data==t)
+                return prev_;
+
+            iter=next_;
+            next_=iter->next;
+            next_->prev=iter;
+
+            iter=prev_;
+            prev_=iter->prev;
+            prev_->next=iter;
+        }
+        return NULL;
+    }
+}
+
+
+template<typename T>
+bool list<T>::removeElement(const T& t)
+{
+    if( this->empty() )
+        return false;
+    node* iter;
+    node* next_;
+    node* aux;
+    next_=this->first;
+
+    for(size_t i=1; i<= this->max_size; i++)
+    {
+        iter=next;
+        if((next_->data)==t)
+        {   
+            aux=next_->prev;
+            iter=next_->next;
+            delete next_;
+            aux->next=iter;
+            iter->prev=aux;
+            return true;
+        }
+        next_=iter->next;
+        next_->prev=iter;       
+    }
+    return false; //Si no lo encontro en el for es porque no esta en la lista
+}
+
+
+// list const &operator=(const list<T>& other_list)
+// {
+
+// }
+
+
+// template<typename TT>
+// ostream& list<TT>::operator<<(ostream& os, list<TT>& L); 
+// {
+//     //El orden de impresion es desde el primero hasta el utlimo
+//     if(L->empty())
+//         return os;
+//     node* iter;
+//     node* next_;
+
+//     next_=L->first
+//     for(size_t i=1;i<=L->max_size;i++)
+//     {
+//         iter=next_;
+//         os<< next_->data;
+//         next_=iter->next;
+//         next_->prev=iter;
+//     }
+//     return os;
+// }
+
+#endif // _LIST_H_
+
+// template<typename T>
+// bool list<T>::swap(const size_t& n1=1,const size_t& n2=2)
+// {
+    //Serviría para el ejercicio 4 de listas circulares. 
+// }
---
+++ Codigos/dictionary.h
@@ -0,0 +1,238 @@
+#ifndef _DICTIONARY_H_
+#define _DICTIONARY_H_
+
+#include <iostream>
+#include <string.h>
+#include <cstring>
+#include "Array.h"
+#include "tools.h"
+#include "block.h"
+#include "sha256.h"
+
+#define STR_INIT "init"
+#define STR_TRANSFER "transfer"
+#define STR_MINE "mine"
+#define STR_BALANCE "balance"
+#define STR_BLOCK "block"
+#define STR_TXN "txn"
+#define STR_LOAD "load"
+#define STR_SAVE "save"
+
+#define NULL_HASH "0000000000000000000000000000000000000000000000000000000000000000"
+#define MAXCMD 8
+
+#define MSG_FAIL "FAIL"
+
+#define CHOP_SIZE 5
+using namespace std;
+
+typedef string (* p_func)(Array <string>);
+
+string cmdInit(Array <string> args);
+string cmdTransfer(Array <string> args);
+string cmdMine(Array <string> args);
+string cmdBalance(Array <string> args);
+string cmdBlock(Array <string> args);
+string cmdTxn(Array <string> args);
+string cmdLoad(Array <string> args);
+string cmdSave(Array <string> args);
+
+
+struct cmd_option_t
+{
+	string name;
+	p_func comand;
+};
+
+static cmd_option_t dictionary_cmd[] = {
+	{STR_INIT, cmdInit},
+	{STR_TRANSFER, cmdTransfer},
+	{STR_MINE, cmdMine},
+	{STR_BALANCE, cmdBalance},
+	{STR_BLOCK, cmdBlock},
+	{STR_TXN, cmdTxn},
+	{STR_LOAD, cmdLoad},
+	{STR_SAVE, cmdSave},
+};
+
+p_func dictCmds(const string cmd)
+{
+	string aux;
+	int i = 0;
+
+	while(cmd != dictionary_cmd[i].name && i < MAXCMD) i++;
+	
+	if(i == MAXCMD)
+	{
+		cerr << "El comando no es valido" << endl;
+		exit(1);
+	}
+	return dictionary_cmd[i].comand;
+}
+
+
+string cmdInit(Array <string> args)
+{
+
+	string user;
+	string STR_TXNing;
+	double value;
+	size_t bits;
+
+	user =sha256(args[0]);
+
+	if(isNumber<double>(args[1]) == false || args[1][0] == '-')
+	{
+		cerr << "El user no es valido" << endl;
+		exit(1);
+	}
+
+	value = stod(args[1]);
+	
+	if(isNumber<int>(args[2]) == false || args[2][0] == '-')
+	{
+		cerr << "El bits no es valido" << endl;
+		exit(1);
+	}
+
+	bits = stoi(args[2]);
+
+	STR_TXNing.append("1");
+	STR_TXNing.append("\n");
+	STR_TXNing.append(NULL_HASH);
+	STR_TXNing.append(" ");
+	STR_TXNing.append("0");
+	STR_TXNing.append(" ");
+	STR_TXNing.append(NULL_HASH);
+	STR_TXNing.append("\n");
+	STR_TXNing.append("1");
+	STR_TXNing.append("\n");
+	STR_TXNing.append(args[1]);
+	STR_TXNing.append(" ");
+	STR_TXNing.append(user);
+	
+	istringstream iss(STR_TXNing);
+
+	block genesis_block(NULL_HASH, bits, &iss);
+	return sha256(genesis_block.getBlockAsString());
+}
+
+string cmdTransfer(Array <string> args)
+{
+	//Se debe buscar la ultima aparicion de ese usuario primero en la MEMPOOL y si no se encuentra nada, en la 
+	//ALGOCHAIN; conseguir su value y verificar que dicho valor (su dinero disponible) no sea menor a la suma de
+	//las cantidades a transferir.
+
+	//Otra alternativa es ir realizando las transacciones y si se queda sin dinero el usuario source, cancelar las
+	// transacciones y arrojar un error.
+
+	istringstream iss((sha256(args[0])); //El primer elemento se condice con el usuario de origen.
+	string src,src_value,aux
+	Array<string> dst(1); //Inicializacion por defecto en uno
+	Array<string> dst_value(1); //Inicializacion por defecto en uno
+	double scr_value=100; //Se inicializa en 100 para pruebas. Debe obtenerse de la mempool o algochain segun corresp.
+	Array <double> dst_value(1);
+	size_t i=0;
+
+	//Se obtienen el hash del usuario origen y su dinero disponible indicados por linea de comandos en variables aux.
+	//Se verifica en la mempool y/o algochain si las transacciones son validas (mediante variables aux). Si lo son,
+	// se cargan. Si no ,se anulan.
+
+	if( !(getline(iss, src, ' ').good()) || !(iss.eof()))
+		return MSG_FAIL;
+
+	//HAY QUE OBTENER scr_value PARA SEGUIR A PARTIR DE ESTE PUNTO
+	//src_value= ;
+	while(getline(iss, aux, ' ').good() || iss.eof())
+	{
+		//Se consiguen los hash de los usuarios destino y los valores a transferir
+		if(dst.getSize()==(i+1))
+		{
+			dst.ArrayRedim( (i+1)+CHOP_SIZE);
+			dst_value.ArrayRedim( (i+1)+CHOP_SIZE);
+		}
+		if( !(getline(iss, dst[i], ' ').good()) || !(iss.eof()))
+			return MSG_FAIL;
+		if( !(getline(iss, dst_value[i], ' ').good()) || !(iss.eof()))
+			return MSG_FAIL;
+		if(dst_value[i]<0)
+			return MSG_FAIL;
+		dst_value[i]=stod(dst_value[i]);
+		scr_value-=dst_value[i];
+		i++;
+	}
+	if(src_value<0)
+		return MSG_FAIL;
+	
+	return "hola";
+}
+
+string cmdMine(Array <string> args)
+{
+	return "hola";
+}
+
+string cmdBalance(Array <string> args)
+{
+	return "hola";
+}
+
+string cmdBlock(Array <string> args)
+{
+	return "hola";
+}
+
+string cmdTxn(Array <string> args)
+{
+	return "hola";
+}
+
+string cmdLoad(Array <string> args)
+{
+	return "hola";
+}
+
+string cmdSave(Array <string> args)
+{
+	return "hola";
+}
+
+
+Array <string> parseCmdArgs(string str, size_t N)//funcion para verificar la correcta escritura de los argumentos, 
+									  // devuelve un array dinamico con los argumentos.
+									 //  Le paso N para saber cuantos argumentos debo mirar para que sea correcto
+									//   Si es N=0 entonces es argumentos variables impares minimo 3
+{
+	istringstream iss(str);
+	size_t i=0;
+	string aux;
+
+	Array <string> args(1); // Lo dimensiono con 3 porque es el valor minimo, despues lo resize
+	while(getline(iss, aux, ' ').good() || iss.eof() )//averiguar que pasa con el ultimo
+	{
+		if(aux == "")
+		{
+			cerr << "Hay un espacio de mas" << endl; // poner error
+			exit(1);
+		}
+		if (i>=args.getSize())
+		{
+			args.ArrayRedim(args.getSize()+1);
+		}
+		if(iss.eof()==true)
+		{
+			args[i]=aux;
+			break;
+		}
+		args[i]=aux;
+		i++;
+	}
+	if(N!=(i+1) && N!=0)
+	{
+		cerr << "Numero incorrecto de argumentos"<< endl;
+		exit(1);
+	}
+	return args;
+}
+
+#endif //_DICTIONARY_H_
